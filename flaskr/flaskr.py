d289fe3f8bcd80ade08f18bf4bd92961ac661c11
Ali Kaafarani <ali@kvikshaug.no>
2015-05-20 14:41:24 +0200

    Remove unused imports

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -1,7 +1,6 @@
-import sqlite3
 import os
 
-from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from flask import Flask
 
 app = Flask(__name__)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
