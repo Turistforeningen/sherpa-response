614396392106c1e1b44d8b12ae498da203a8e6e6
Ali Kaafarani <ali@kvikshaug.no>
2015-05-20 15:00:53 +0200

    Add initial index view

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -1,9 +1,13 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host=os.environ['APP_HOST'], port=int(os.environ['APP_PORT']))
