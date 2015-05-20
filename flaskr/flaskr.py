a9865a4d5f1be1e2b8fb36fdf07221909f836d07
Ali Kaafarani <ali@kvikshaug.no>
2015-05-20 13:54:11 +0200

    Run app on execute

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -4,3 +4,6 @@ from flask import Flask, request, session, g, redirect, url_for, abort, render_t
 
 app = Flask(__name__)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+if __name__ == '__main__':
+    app.run()
