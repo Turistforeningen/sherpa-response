8cce15b25bf3e0d0a5fb22546960f35192b52c78
Ali Kaafarani <ali@kvikshaug.no>
2015-05-20 14:22:11 +0200

    Enable dev environment

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,9 @@ FROM python:2.7
 
 WORKDIR /sherparesponse
 ENV HOME /root
-EXPOSE 8000
 RUN apt-get -y autoclean && apt-get -y autoremove && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
-COPY . /sherparesponse/
+COPY requirements.txt /sherparesponse/
 RUN pip install --src /tmp -r /sherparesponse/requirements.txt
+
+COPY . /sherparesponse/
--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -1,4 +1,5 @@
 import sqlite3
+import os
 
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 
@@ -6,4 +7,4 @@ app = Flask(__name__)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host=os.environ['APP_HOST'], port=int(os.environ['APP_PORT']))
